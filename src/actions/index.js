4ffee0c9a9f6b900a78d73a7b082d979485f3c16
William Gilbert Go <williamgilbertgo@gmail.com>
2019-12-02 18:39:00 -0800

    Update API Key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // Auth from OpenWeatherMap API
-const API_KEY = 'f1d37bfbc7711f9f8e3a0940bf6e8a28';
+// const API_KEY = 'f1d37bfbc7711f9f8e3a0940bf6e8a28';
+const API_KEY= 'e12cda1d493571d458702f3562348864';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather/?appid=${API_KEY}`; 
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
