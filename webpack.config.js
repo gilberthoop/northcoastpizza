3332bf2182f8cebb9e4983b4ce1f3e8c26eed9a7
William Gilbert Go <williamgilbertgo@gmail.com>
2019-12-02 18:44:56 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,37 @@
+var webpack = require("webpack");
+
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: ["./src/index.js"],
   output: {
     path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    publicPath: "/",
+    filename: "bundle.js"
   },
   module: {
     loaders: [
       {
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: "babel",
         query: {
-          presets: ['react', 'es2015', 'stage-1']
+          presets: ["react", "es2015", "stage-1"]
         }
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ["", ".js", ".jsx"]
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './',
+    contentBase: "./",
     watchOptions: {
       aggregateTimeout: 300,
       poll: 1000
     }
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
-
-var webpack = require('webpack');
-
-plugins: [
-  new webpack.DefinePlugin({
-    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-  })
-];
